79761c31f9f0232abe1a5b8ccba2e6e16854da56
Thiago Carvalho <thiago.oak@gmail.com>
2020-04-16 20:52:07 +0200

    Add VSCODE Settings

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 pushd `dirname $0` > /dev/null
 SCRIPTPATH=`pwd`
 popd > /dev/null
@@ -6,8 +7,9 @@ pushd ~ > /dev/null
 
 ln -sf "$SCRIPTPATH/config/gitconfig" .gitconfig
 ln -sf "$SCRIPTPATH/config/gitignore" .gitignore
-git config --global core.excludesfile '~/.gitignore'
 
 ln -sf "$SCRIPTPATH/config/vimrc" .vimrc
+
+ln -sf "$SCRIPTPATH/config/vscode-settings.json" $HOME/Library/Application\ Support/Code/User/settings.json
 
 popd > /dev/null
