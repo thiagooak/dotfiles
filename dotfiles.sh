63a4365d16ac91a45d32d5ac1bcb30403efbb190
Thiago Carvalho <thiago.oak@gmail.com>
2020-04-20 07:45:01 +0200

    Check for updates on startup

---
+++ dotfiles.sh
@@ -0,0 +1,5 @@
+#ZSH_THEME="ys"
+eval "$(ssh-agent -s)"
+export PATH="$HOME/src/dotfiles/bin:$PATH"
+ssh-add
+dotfiles
--- setup.sh
+++ setup.sh
@@ -13,3 +13,11 @@ ln -sf "$SCRIPTPATH/config/vimrc" .vimrc
 ln -sf "$SCRIPTPATH/config/vscode-settings.json" $HOME/Library/Application\ Support/Code/User/settings.json
 
 popd > /dev/null
+
+echo $PATH | grep "dotfiles/bin" > /dev/null
+if [ $? -ne 0 ]
+    then
+        echo "Add source \$HOME/src/dotfiles/dotfiles.sh to .[zsh]rc"
+fi
+
+echo "Done!"
